511356cd931cd066a700129ff960a4b83685b318
Percy-Potter2008 <75735895+Percy-Potter2008@users.noreply.github.com>
2021-06-17 19:10:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -33,6 +33,9 @@ Webcam.set({ width:360, height:250, image_format : 'jpeg', jpeg_quality:90 });
   document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
   });
  }
+Webcam.upload( base64image, 'upload.php', function(code, text) {
+ 
+});
 function save() {
  link = document.getElementById("link");
   image = document.getElementById("selfie_image").src;
